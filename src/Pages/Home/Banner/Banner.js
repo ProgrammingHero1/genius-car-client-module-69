f7023b6c2ed9c6e1bda064899da9dbab17c6caeb
Programming Hero <programming.hero1@gmail.com>
2022-10-28 00:09:57 -0500

    set banner text and buttons

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -11,8 +11,22 @@ const Banner = () => {
         <div className="carousel w-full">
             <div id="slide1" className="carousel-item relative w-full">
                 <img src={img1} alt="" className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" className="btn btn-circle">❮</a>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
+                    <h1 className='text-6xl font-bold text-white'>
+                        Affordable <br />
+                        Price for Car <br />
+                        Servicing
+                    </h1>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 w-2/5 left-24 top-1/2">
+                    <p className='text-xl text-white'>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                </div>
+                <div className="absolute flex justify-start transform -translate-y-1/2 w-2/5 left-24 top-3/4">
+                <button className="btn btn-warning mr-5">Warning</button>
+                <button className="btn btn-outline btn-warning">Warning</button>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide4" className="btn btn-circle mr-5">❮</a>
                     <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
     </>
 
     return (
-        <div className="navbar h-20 mb-12 bg-base-100">
+        <div className="navbar h-20 mb-12 pt-12 bg-base-100">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
