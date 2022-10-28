eedb4b5cd290d5673e8b4bfb002aeefd90429c01
Programming Hero <programming.hero1@gmail.com>
2022-10-28 00:32:10 -0500

    banner image gradient overlay and dynamic data

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,52 +5,57 @@ import img3 from '../../../assets/images/banner/3.jpg';
 import img4 from '../../../assets/images/banner/4.jpg';
 import img5 from '../../../assets/images/banner/5.jpg';
 import img6 from '../../../assets/images/banner/6.jpg';
+import './Banner.css';
+import BannerItem from './BannerItem';
+
+const bannerData = [
+    {
+        image: img1,
+        id: 1,
+        prev: 6,
+        next: 2
+    },
+    {
+        image: img2,
+        id: 2,
+        prev: 1,
+        next: 3
+    },
+    {
+        image: img3,
+        id: 3,
+        prev: 2,
+        next: 4
+    },
+    {
+        image: img4,
+        id: 4,
+        prev: 3,
+        next: 5
+    },
+    {
+        image: img5,
+        id: 5,
+        prev: 4,
+        next: 6
+    },
+    {
+        image: img6,
+        id: 6,
+        prev: 5,
+        next: 1
+    }
+]
 
 const Banner = () => {
     return (
         <div className="carousel w-full">
-            <div id="slide1" className="carousel-item relative w-full">
-                <img src={img1} alt="" className="w-full" />
-                <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
-                    <h1 className='text-6xl font-bold text-white'>
-                        Affordable <br />
-                        Price for Car <br />
-                        Servicing
-                    </h1>
-                </div>
-                <div className="absolute flex justify-end transform -translate-y-1/2 w-2/5 left-24 top-1/2">
-                    <p className='text-xl text-white'>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
-                </div>
-                <div className="absolute flex justify-start transform -translate-y-1/2 w-2/5 left-24 top-3/4">
-                <button className="btn btn-warning mr-5">Warning</button>
-                <button className="btn btn-outline btn-warning">Warning</button>
-                </div>
-                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-                    <a href="#slide4" className="btn btn-circle mr-5">❮</a>
-                    <a href="#slide2" className="btn btn-circle">❯</a>
-                </div>
-            </div>
-            <div id="slide2" className="carousel-item relative w-full">
-                <img src={img2} className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide1" className="btn btn-circle">❮</a>
-                    <a href="#slide3" className="btn btn-circle">❯</a>
-                </div>
-            </div>
-            <div id="slide3" className="carousel-item relative w-full">
-                <img src="https://placeimg.com/800/200/arch" className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide2" className="btn btn-circle">❮</a>
-                    <a href="#slide4" className="btn btn-circle">❯</a>
-                </div>
-            </div>
-            <div id="slide4" className="carousel-item relative w-full">
-                <img src="https://placeimg.com/800/200/arch" className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide3" className="btn btn-circle">❮</a>
-                    <a href="#slide1" className="btn btn-circle">❯</a>
-                </div>
-            </div>
+            {
+                bannerData.map(slide => <BannerItem
+                    key={slide.id}
+                    slide={slide}
+                ></BannerItem>)
+            }
         </div>
     );
 };
