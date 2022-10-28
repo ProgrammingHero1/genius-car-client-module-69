47759e2f5321650403e5e6f9d112d45080d7985e
Programming Hero <programming.hero1@gmail.com>
2022-10-27 21:20:36 -0500

    added simple header

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/logo.svg';
 
 const Header = () => {
+
+    const menuItems = <>
+        <li><Link to='/'>Home</Link></li>
+    </>
+
     return (
-        <div>
-            <h2>Header</h2>
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {menuItems}
+                    </ul>
+                </div>
+                <Link to="/" className="btn btn-ghost normal-case text-xl">
+                    <img src={logo} alt="" />
+                </Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
+                    {menuItems}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Get started</a>
+            </div>
         </div>
     );
 };
