952241a3dc630c1c9dde89e7334a8687f08db325
Programming Hero <programming.hero1@gmail.com>
2022-10-29 15:02:40 -0500

    added the login page

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import img from '../../assets/images/login/login.svg';
+
+const Login = () => {
+
+    const handleLogin = event =>{
+        event.preventDefault();
+    }
+
+    return (
+        <div className="hero w-full my-20">
+            <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
+                <div className="text-center lg:text-left">
+                    <img className='w-3/4' src={img} alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <h1 className="text-5xl text-center font-bold">Login</h1>
+                    <form onSubmit={handleLogin} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="Login" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,5 +1,6 @@
 import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
 
 const { createBrowserRouter } = require("react-router-dom");
 
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
         {
             path: '/',
             element: <Home></Home>
+        },
+        {
+          path: '/login', 
+          element: <Login></Login>
         }
       ]
     }
