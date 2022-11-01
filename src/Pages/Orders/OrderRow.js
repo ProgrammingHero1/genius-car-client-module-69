0f63e7726ba2c4b0cb381f899da584fbf54c224d
Programming Hero <programming.hero1@gmail.com>
2022-11-01 01:24:32 -0500

    display orders in a table

---
+++ src/Pages/Orders/OrderRow.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+
+const OrderRow = ({ order }) => {
+    const { serviceName, phone, customer, price, service } = order;
+    const [orderService, setOrderService] = useState({})
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/services/${service}`)
+            .then(res => res.json())
+            .then(data => setOrderService(data));
+    }, [service])
+
+    return (
+        <tr>
+            <th>
+                <label>
+                    <button className='btn btn-ghost'>X</button>
+                </label>
+            </th>
+            <td>
+                <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                        <div className="rounded w-24 h-24">
+                            {
+                                orderService?.img && 
+                                <img src={orderService.img} alt="Avatar Tailwind CSS Component" />}
+                        </div>
+                    </div>
+                    <div>
+                        <div className="font-bold">{customer}</div>
+                        <div className="text-sm opacity-50">{phone}</div>
+                    </div>
+                </div>
+            </td>
+            <td>
+                {serviceName}
+                <br />
+                <span className="badge badge-ghost badge-sm">${price}</span>
+            </td>
+            <td>Purple</td>
+            <th>
+                <button className="btn btn-ghost btn-xs">details</button>
+            </th>
+        </tr>
+    );
+};
+
+export default OrderRow;
--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,19 +1,45 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+import OrderRow from './OrderRow';
 
 const Orders = () => {
-    const {user} = useContext(AuthContext);
-    const [orders, setOrders ] = useState({})
-    
-    useEffect(() =>{
+    const { user } = useContext(AuthContext);
+    const [orders, setOrders] = useState({})
+
+    useEffect(() => {
         fetch(`http://localhost:5000/orders?email=${user.email}`)
-        .then(res => res.json())
-        .then(data =>setOrders(data))
+            .then(res => res.json())
+            .then(data => setOrders(data))
     }, [user?.email])
 
     return (
         <div>
             <h2 className="text-5xl">You have {orders.length} Orders</h2>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map(order => <OrderRow
+                                key={order._id}
+                                order={order}
+                            ></OrderRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
