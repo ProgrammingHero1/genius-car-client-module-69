77b6f0c819ce8a391b3d967af3605f285b60c1cf
Programming Hero <programming.hero1@gmail.com>
2022-11-02 22:33:07 -0500

    get jwt token from server and store in LS

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -5,25 +5,47 @@ import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Login = () => {
 
-    const {login} = useContext(AuthContext);
+    const { login } = useContext(AuthContext);
     const location = useLocation();
     const navigate = useNavigate();
 
     const from = location.state?.from?.pathname || '/';
 
-    const handleLogin = event =>{
+    const handleLogin = event => {
         event.preventDefault();
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
 
         login(email, password)
-        .then( result => {
-            const user = result.user;
-            console.log(user);
-            navigate(from, {replace: true});
-        })
-        .then(error => console.log(error));
+            .then(result => {
+                const user = result.user;
+                
+
+                const currentUser = {
+                    email: user.email
+                }
+
+                console.log(currentUser);
+
+                // get jwt token
+                fetch('http://localhost:5000/jwt', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(currentUser)
+                })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    // local storage is the easiest but not the best place to store jwt token
+                    localStorage.setItem('genius-token', data.token);
+                })
+
+                // navigate(from, { replace: true });
+            })
+            .catch(error => console.log(error));
     }
 
     return (
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -32,7 +32,7 @@ const router = createBrowserRouter([
         },
         {
           path: '/orders',
-          element: <Orders></Orders>
+          element: <PrivateRoute><Orders></Orders></PrivateRoute>
         }
       ]
     }
